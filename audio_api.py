775a786e6bec9e2b26777ad484eacca3a097ba08
sanskruti123987 <sanskrutisingh029@gmail.com>
2025-11-03 11:29:18 +0530

    This is the final audio_api

--- audio_api.py
+++ audio_api.py
@@ -1,12 +1,15 @@
-from flask import Flask, request, jsonify
-import your_assistant_script
+from flask import Flask, request, jsonify                                      # server, reads incoming data from frontend, sends json response back to frontend
+import your_assistant_script                                                 # import main voice assistant logic file
 
-app = Flask(__name__)
+app = Flask(__name__)                                                     #initialises flask web app
 
-@app.route("/process", methods=["POST"])
-def process():
-    text = request.json["text"]
-    reply = your_assistant_script.process_command(text)
-    return jsonify({"reply": reply})
+@app.route("/process", methods=["POST"])                                        #dehines endpoint route where input is sent
+def process():                                                       
+    text = request.json["text"]                                                   #reads recognised speech text (from js)
+    reply = your_assistant_script.process_command(text)                                # passes text here 
+    return jsonify({"reply": reply})                                               # sends processed response back to frontend as json
 
-app.run(port=5000)
+app.run(port=5000)                                    # starts server allows html page to connect it
+
+#serves as backend interface for voice assistant
+#this is used to give response back to the user
